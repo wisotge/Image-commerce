6cb2fcea0c62b6dba10bdeac27ef3d2ab339801e
rnasltn@ajou.ac.kr <rnasltn@ajou.ac.kr>
2019-12-06 04:16:20 -0800

    Merge main#sort into main#index

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,10 +3,6 @@ class MainController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @image_item = ImageItem.order("created_at DESC").page(params[:page]).per(9)
-  end
-
-  def sort
     sort_type = params[:type]
     if sort_type == "pay"
       @image_item = ImageItem.where(imgtype: "pay").page(params[:page]).per(9)
@@ -15,17 +11,19 @@ class MainController < ApplicationController
     elsif sort_type == "price"
       order = params[:order]
       if order == "high"
-        @image_item = ImageItem.all.order("price DESC").page(params[:page]).per(9)
+        @image_item = ImageItem.where(imgtype: "pay").order("price DESC").page(params[:page]).per(9)
       else
-        @image_item = ImageItem.all.order("price ASC").page(params[:page]).per(9)
+        @image_item = ImageItem.where(imgtype: "pay").all.order("price ASC").page(params[:page]).per(9)
       end
-    else
+    elsif sort_type == "date"
       order = params[:order]
       if order == "recent"
         @image_item = ImageItem.all.order("created_at DESC").page(params[:page]).per(9)
       else
         @image_item = ImageItem.all.order("created_at ASC").page(params[:page]).per(9)
       end
+    else
+      @image_item = ImageItem.order("created_at DESC").page(params[:page]).per(9)
     end
   end
 
