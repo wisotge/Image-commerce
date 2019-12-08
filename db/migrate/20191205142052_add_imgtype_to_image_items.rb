0d9bcea998a698f13c0a9a026744d8ef74288286
Jihwan Kim <noneobj@gmail.com>
2019-12-08 09:09:39 +0900

    Remove fucking migration file

--- db/migrate/20191205142052_add_imgtype_to_image_items.rb
+++
@@ -1,5 +0,0 @@
-class AddImgtypeToImageItems < ActiveRecord::Migration[5.2]
-  def change
-    add_column :image_items, :imgtype, :string
-  end
-end
--- db/migrate/20191205142250_delete_type_from_image_items.rb
+++
@@ -1,5 +0,0 @@
-class DeleteTypeFromImageItems < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :image_items, :type
-  end
-end
